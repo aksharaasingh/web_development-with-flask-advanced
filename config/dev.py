ee59cd28bd6c4708e7a7511eb539376a46a72341
aksharaasingh <AKSHARA@149911@dpsgurgaon.org>
2018-11-18 17:24:08 +0530

    made corrections

--- config/dev.py
+++
@@ -1,6 +0,0 @@
-DEBUG = False
-
-SECRET_KEY = 'topsecret'
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:topsecret@localhost/catalog_db'
-SQLALCHEMY_TRACK_MODIFICATIONS=False
-
--- config/prod.py
+++ config/prod.py
@@ -1,6 +1,8 @@
+import os
+
 DEBUG = False
 
 SECRET_KEY='topsecret'
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:topsecret@localhost/catalog_db'
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS=False
 
