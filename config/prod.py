ae7a7296fefb1bd77caccc356e8b1bdf0aa4a431
rollno4 <akshara748.singh@gmail.com>
2019-05-02 18:33:45 +0530

    app

--- config/prod.py
+++ config/prod.py
@@ -3,5 +3,5 @@ import os
 DEBUG = True
 
 SECRET_KEY='topsecret'
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:topsecret@localhost/catalog_db'
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS=False
